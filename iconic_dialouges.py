75d4a5bfe78072feb0d873a3b544c73f304eb0a7
lucifertrj <jaintarun.abd17@gmail.com>
2021-06-21 08:51:41 +0530

    📝 📝 dialouges refrence code

---
+++ iconic_dialouges.py
@@ -0,0 +1,17 @@
+iconic_dialouges_naruto = {
+    "Naruto":"When you are alive you need a reason for your existence, Being Unable to find one is same as being dead",
+    "Sasuke":"I shut my eyes a long time ago... the things I seek... now lies only in the darkness",
+    "Itachi":"Dont be so quick..., to judge me. After all you only see what i choose to show you",
+    "Madara":"Wake up to reality... nothing ever goes as planned in this accursed world... the longer you live the more you realize the only thing that truly exist in this reality is pain, suffering and futility.",
+    "Orochimaru":"Its the human nature not to realize the true value of something unless they lose it",
+    "Jiraiya":"A place where someone still thinks about you is a place you can call home",
+    "Kakashi":"In society those who don't have many abilities, tend to complain more",
+    "Guy":"You are right all efforts are pointless, if you don't belive in yourself",
+    "Lee":"Hardwork beats natural talent",
+    "Pain":"When i had nothing and no one... i always had pain",
+    "Nagato":"Those who do not understand true pain, can never understand true peace",
+    "Obito":"When a man learns to love, he must bear the risk of hatred",
+    "Shikamaru":"Sometimes I wish I was just a cloud, floating along",
+    "Neji":"The difference between stupidity and genius, is that genius has its limits",
+    "Gaara":"I finally understand I'm alone. I won't believe in anyone or love anyone. I am alone"
+}
